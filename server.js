336f1a5f4d1642d86b286a64d7a0995b02ea6c24
Arjun Singh <arjundeakinuni@gmail.com>
2025-10-01 07:49:19 +1000

    JF_10

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`SafeNet API listening on ${PORT} - server.js:4`));
+app.listen(PORT, '0.0.0.0', () =>
+  console.log(`SafeNet API listening on ${PORT} - server.js:5`)
+);
